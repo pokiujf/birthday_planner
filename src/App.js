6887ceb659571091b8c263648871babbdd6eb7c7
A Machalica <a.machalica@selleo.com>
2019-05-29 13:04:58 +0200

    Setup user and user listing components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
-import LoginPage from "./components/loginPage";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import UserListing from "./components/UserListing";
+import User from "./components/User";
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +14,9 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <PrivateRoute path="/" component={UserListing}/>
+        <PrivateRoute path="/users" component={UserListing}/>
+        <PrivateRoute path="/users/:id" component={User}/>
         <Route path="/login" component={LoginPage}/>
       </Router>
     );
