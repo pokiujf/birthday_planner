ff327cf1a4e4b1514cd5669418a8f864afdd94ca
Mattias Hilliges <mattias.hilliges@td-berlin.com>
2019-05-29 16:08:13 +0200

    add class to NewUser.js

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -41,69 +41,71 @@ export default class NewUser extends Component {
 
   render() {
     return (
-      <form>
-        <div className="row">
-          <div className="col-3 offset-2">
-            {
-              this.state.image
-                ? <img src={this.state.image} />
-                : 'Placeholder here'
-            }
+      <div className="new-user-container">
+        <form>
+          <div className="row">
+            <div className="col-3 offset-2">
+              {
+                this.state.image
+                  ? <img src={this.state.image} />
+                  : 'Placeholder here'
+              }
 
+            </div>
+            <div className="col-7">
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  name="name"
+                  id="name"
+                  value={this.state.name}
+                  placeholder="Full name"
+                  onChange={this.updateField}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  name="email"
+                  id="email"
+                  type="email"
+                  value={this.state.email}
+                  placeholder="Email"
+                  onChange={this.updateField}
+                />
+              </div>
+              <div>
+                <label htmlFor="date">Date in format (1999-12-31)</label>
+                <input
+                  name="date"
+                  id="date"
+                  value={this.state.date}
+                  placeholder="Date of birth YYYY-MM-DD"
+                  onChange={this.updateField}
+                  required
+                />
+                {this.state.errors.date && (
+                  <div style={{color: 'red'}}>{this.state.errors.date}</div>
+                )}
+              </div>
+              <div>
+                <label htmlFor="image">Image URL</label>
+                <input
+                  name="image"
+                  id="image"
+                  value={this.state.image}
+                  placeholder="Image url"
+                  onChange={this.updateField}
+                />
+              </div>
+              <div>
+                <button type="submit" onClick={this.saveUser}>Add user</button>
+              </div>
+            </div>
           </div>
-          <div className="col-7">
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                name="name"
-                id="name"
-                value={this.state.name}
-                placeholder="Full name"
-                onChange={this.updateField}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                name="email"
-                id="email"
-                type="email"
-                value={this.state.email}
-                placeholder="Email"
-                onChange={this.updateField}
-              />
-            </div>
-            <div>
-              <label htmlFor="date">Date in format (1999-12-31)</label>
-              <input
-                name="date"
-                id="date"
-                value={this.state.date}
-                placeholder="Date of birth YYYY-MM-DD"
-                onChange={this.updateField}
-                required
-              />
-              {this.state.errors.date && (
-                <div style={{color: 'red'}}>{this.state.errors.date}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="image">Image URL</label>
-              <input
-                name="image"
-                id="image"
-                value={this.state.image}
-                placeholder="Image url"
-                onChange={this.updateField}
-              />
-            </div>
-            <div>
-              <button type="submit" onClick={this.saveUser}>Add user</button>
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
