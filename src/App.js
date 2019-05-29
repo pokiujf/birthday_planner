391789d2146536c8699c4966898590f82a23a578
A Machalica <a.machalica@selleo.com>
2019-05-29 12:18:31 +0200

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
+import LoginPage from "./components/loginPage";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+
   }
 
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+        <Route path="/login" component={LoginPage}/>
+      </Router>
     );
   }
 }
 
 export default App;
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("user") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
---
+++ src/components/loginPage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class LoginPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      email: '',
+      errors: {},
+    };
+  }
+
+  submitLoginForm = (event) => {
+    event.preventDefault();
+    axios.get(
+      'http://localhost:3001/users',
+      {
+        params: {
+          email: this.state.email,
+        }
+      }).then((response) => {
+      let foundUser = response.data[0];
+      if (foundUser) {
+        localStorage.setItem('user', JSON.stringify(foundUser));
+        this.setState({ errors: {}})
+      } else {
+        this.setState({ errors: { email: 'No user for this email' } })
+      }
+    });
+  };
+
+  updateField = ({ target: { value, name } }) => {
+    this.setState({ [name]: value })
+  };
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id='email' name="email" value={this.state.email} onChange={this.updateField} />
+          {this.state.errors.email && (
+            <div style={{color: 'red'}}>{this.state.errors.email}</div>
+          )}
+        </div>
+        <button type="submit" onClick={this.submitLoginForm}>Login</button>
+      </form>
+    );
+  }
+}
